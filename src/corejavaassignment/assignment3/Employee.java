ccbb02ffe5d4cc5d6ec8eb48d54c13ca8711384f
Roneet Michael <roneetmichael@yahoo.com>
2021-10-20 18:47:44 +0530

    Started Assignment 3

---
+++ src/corejavaassignment/assignment3/Employee.java
@@ -0,0 +1,72 @@
+package corejavaassignment.assignment3;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
+    String dateOfBirth;
+
+    public Employee(Long id, String name, String email, int age, String dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Employee() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name= '" + name + '\'' +
+                ", email= '" + email + '\'' +
+                ", age= " + age +
+                ", dateOfBirth= " + dateOfBirth +
+                '}';
+    }
+}
