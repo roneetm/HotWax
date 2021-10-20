f8bdea6d1d176519c185a5854bab5e5127447274
Roneet Michael <roneetmichael@yahoo.com>
2021-10-20 16:47:54 +0530

    Reading words from file with frequency

--- src/corejavaassignment/assignment1/MainClass.java
+++ src/corejavaassignment/assignment1/MainClass.java
@@ -1,9 +1,37 @@
 package corejavaassignment.assignment1;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class MainClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        File file = new File("src/corejavaassignment/assignment1/Words.txt");
+        HashMap<String, Integer> map = new HashMap<>();
+        try (FileReader fileReader = new FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(fileReader);) {
+            String line;
+            while((line = bufferedReader.readLine()) != null){
 
+                if(map.containsKey(line)) {
+                    map.put(line, map.get(line)+1);
+                }
+                else {
+                    map.put(line, 1);
+                }
+                //System.out.println(line);
+            }
+        }
+        catch (IOException ioException){
+            ioException.printStackTrace();
+        }
 
+        Map<String, Integer> hashMap = new TreeMap<>(map);
+//        for(Map.Entry<String, Integer> entry : map.entrySet() ){
+//            System.out.println(entry);
+//        }
+        System.out.println(hashMap);
     }
 }
