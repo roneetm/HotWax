b303e7cc4e417c11acb46b2fcd3020d3f01a0a60
Roneet Michael <roneetmichael@yahoo.com>
2021-10-26 13:07:18 +0530

    Read URL & Print Top Words and Write it in a file

--- src/corejavaassignment/assignment1/MainClass.java
+++ src/corejavaassignment/assignment1/MainClass.java
@@ -1,41 +1,71 @@
 package corejavaassignment.assignment1;
-
 import org.jsoup.Jsoup;
 
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class MainClass {
 
-    public static void readWords() {
+    public static void countWords(String webPageContent) {
         File file = new File("src/corejavaassignment/assignment1/Words.txt");
-        HashMap<String, Integer> map = new HashMap<>();
-        try (FileReader fileReader = new FileReader(file);
-             BufferedReader bufferedReader = new BufferedReader(fileReader);) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
 
-                if (map.containsKey(line)) {
-                    map.put(line, map.get(line) + 1);
+        // Putting webpage content in the Arraylist and cleaning all the break words.
+        ArrayList<String> list = new ArrayList<>();
+        String delimiters = "[-\\s\\t,;.?!:@1234567890$&\\[\\](){}_*/\\+\r\n\"]";
+        String[] words = webPageContent.split(delimiters);
+        list.addAll(Arrays.asList(words));
+        Collections.sort(list);
+
+        //Removing blank spaces from Arraylist
+        list.removeAll(Arrays.asList(" ", null));
+        list.removeAll(Arrays.asList("", null));
+        Map<String, Integer> map = new TreeMap<>();
+
+        // Putting list into Map
+        for(String arr : list){
+            if (map.containsKey(arr)) {
+                    map.put(arr, map.get(arr) + 1);
                 } else {
-                    map.put(line, 1);
+                    map.put(arr, 1);
                 }
-                //System.out.println(line);
-            }
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
         }
 
-        Map<String, Integer> hashMap = new TreeMap<>(map);
-//        for(Map.Entry<String, Integer> entry : map.entrySet() ){
-//            System.out.println(entry);
-//        }
-        System.out.println(hashMap);
+        //Copying Map elements to a sorted TreeMap in reverse order
+        Map<Integer, String> sortedMap = new TreeMap<>(Collections.reverseOrder());
+        for(Map.Entry<String, Integer>  str : map.entrySet()){
+            sortedMap.put(str.getValue(), str.getKey());
+        }
+
+        //Printing the values from the Map.
+
+        Set set = sortedMap.entrySet();
+        Iterator i = set.iterator();
+        int flag = 0;
+        System.out.println("========");
+        while(i.hasNext()){
+            Map.Entry entry = (Map.Entry) i.next();
+            if(flag == 5)
+                break;
+            else {
+                System.out.println(entry.getValue() + " - " + entry.getKey());
+                flag++;
+            }
+        }
+        System.out.println("========");
+
+        // Writing the list to the file
+        try(FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)){
+
+            for(String str : list){
+                bufferedWriter.write(str + System.lineSeparator());
+            }
+
+        }catch (IOException ioException){
+            ioException.getMessage();
+        }
     }
 
     public static String getUrlContent(String url) throws IOException {
@@ -54,6 +84,7 @@ public class MainClass {
 
         //Parsing HTML to text using Jsoup dependency.
         content = Jsoup.parse(content).body().text();
+        content = content.toLowerCase();
         return content;
     }
 
@@ -68,6 +99,7 @@ public class MainClass {
             while ((line = bufferedReader.readLine()) != null) {
                 String url = line;
                 String webpageContent = getUrlContent(url);
+                countWords(webpageContent);
                 System.out.println(webpageContent);
             }
         } catch (FileNotFoundException e) {
@@ -77,8 +109,7 @@ public class MainClass {
         }
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         readURL();
-
     }
 }
