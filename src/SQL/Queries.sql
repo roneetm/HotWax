e357cdc8be343db5fad5c4aa6c0ce311b73b327d
Roneet Michael <roneetmichael@yahoo.com>
2021-11-12 18:47:33 +0530

    Executed 18 SQL Queries. Learnt and Performed Joins Operation on Branch SQL.

--- src/SQL/Queries.sql
+++ src/SQL/Queries.sql
@@ -94,43 +94,56 @@ select * from customers where (rating<=100 AND city='Rome') or rating>100;
 --52) Find all rows from the customer’s table for which the salesperson number is 1001.
 select * from customers natural join salespeople where salespeople.SNUM = 1001;
 --53) Find the total amount in orders for each salesperson where their total of amounts are greater than the amount of the largest order in the table.
+select SNAME, min(AMT) as minAMT from salespeople, orders where salespeople.SNUM = orders.SNUM  group by SNAME;
 --54) Write a query that selects all orders save those with zeroes or NULL in the amount file.
 --55) Produce all combinations of salespeople and customer names such that the former precedes the latter alphabetically, and the latter has a rating of less than 200.
+select * from salespeople natural join customers where rating >= 200;
 --56) Find all salespeople name and commission.
 select sname, comm from salespeople;
 --57) Write a query that produces the names and cities of all customers with the same rating as Hoffman. Write the query using Hoffman’s cnum rather than his rating, so that it would still be usable if his rating is changed.
+select cname,city from customers where rating=(select rating from customers where cnum='2001');
 --58) Find all salespeople for whom there are customers that follow them in alphabetical order.
+select SNAME, CNAME from salespeople natural join customers order by CNAME;
 --59) Write a query that produces the names and ratings of all customers who have average orders.
 select CNAME, RATING from customers natural join orders where AMT > (select AVG(AMT) from orders);
 --60) Find the SUM of all Amounts from the orders table.
 select SUM(AMT) from orders;
 --61) Write a SELECT command that produces the order number, amount, and the date from rows in the order table.
+select ONUM, AMT, ODATE from orders;
 --62) Count the number of non NULL rating fields in the Customers table (including repeats).
 select COUNT(RATING) from customers where rating != NULL;
 --63) Write a query that gives the names of both the salesperson and the customer for each order after the order number.
+select SNAME, CNAME, ONUM from customers, salespeople, orders where customers.SNUM = salespeople.SNUM and customers.CNUM = orders.CNUM;
 --64) List the commissions of all salespeople servicing customers in London.
 select * from salespeople natural join customers where customers.CITY = 'London';
 --65) Write a query using ANY or ALL that will find all salespeople who have no customers located in their city.
+select s.sname, s.city, c.cname, c.city as cus_city from salespeople s,customers c where s.snum=c.snum and s.city!=c.city;
 --66) Write a query using the EXISTS operator that selects all salespeople with customers located in their cities who are not assigned to them.
 --67) Write a query that selects all customers serviced by Peel or Motika. (Hint: The snum field relates the 2 tables to one another.)
+select * from salespeople, customers where salespeople.SNUM = customers.SNUM and salespeople.SNAME in ('Peel', 'Motika');
 --68) Count the number of salespeople registering orders for each day. (If a salesperson has more than one order on a given day, he or she should be counted only once.)
 --69) Find all orders attributed to salespeople who live in London.
 select * from orders natural join salespeople where city = 'London';
 --70) Find all orders by customers not located in the same cities as their salespeople.
+select ONUM, CNAME, SNAME, customers.city, salespeople.city from customers, orders, salespeople where orders.CNUM = customers.cnum AND customers.SNUM = salespeople.SNUM and customers.CITY != salespeople.CITY;
 --71) Find all salespeople who have customers with more than one current order.
 --72) Write a query that extracts from the customer’s table every customer assigned to a salesperson, who is currently having at least one another customer(besides the customer being selected) with orders in the Orders Table.
 --73) Write a query on the customer’s table that will find the highest rating in each city. Put the output in this form: for the city (city), the highest rating is (rating).
-SELECT max(rating), city FROM roneet.customers group by city;
+select city, MAX(rating) as rating from customers, orders where customers.cnum = orders.cnum group by city;
 --74) Write a query that will produce the snum values of all salespeople with orders, having amt greater than 1000 in the Orders Table(without repeats).
+select distinct(salespeople.SNUM) from salespeople, orders where salespeople.SNUM = orders.snum and AMT > 1000 order by snum;
 --75) Write a query that lists customers in a descending order of rating. Output the rating field first, followed by the customer’s names and numbers.
 select rating, cname, cnum from customers order by rating desc;
 --76) Find the average commission for salespeople in London.
 SELECT AVG(COMM) FROM roneet.salespeople;
 --77) Find all orders credited to the same salesperson who services Hoffman.(cnum 2001).
+select c.cname, s.sname, o.onum from customers c, salespeople s, orders o where c.SNUM = s.SNUM and c.CNUM = o.CNUM and c.cnum = 2001;
 --78) Find all salespeople whose commission is in between 0.10 and 0.12(both inclusive).
 SELECT * FROM roneet.salespeople where COMM between 10 AND 12;
 --79) Write a query that will give you the names and cities of all salespeople in London with a commission above 0.10.
+SELECT SNAME, CITY from salespeople where CITY = 'London' and COMM > 10;
 --80) Write a query that selects each customer’s smallest order.
+select CNAME, min(AMT) from customers natural join orders group by CNAME order by min(AMT);
 --81) Write a query that selects the first customer in alphabetical order whose name begins with ‘G’.
 select cname from customers where cname like 'G%' order by cname;
 --82) Write a query that counts the number of different non NULL city values in the customers table.
@@ -148,6 +161,7 @@ select * from salespeople, customers where salespeople.SNUM = customers.SNUM And
 --89) Which salesperson has earned the maximum commission?
 Select SNAME from salespeople where COMM = (select MAX(COMM) from salespeople);
 --90) Does the customer who has placed the maximum number of orders have the maximum rating?
+select rating, onum, count(*) as totalCount from customers, orders where customers.cnum = orders.cnum group by orders.onum order by totalCount desc;
 --91) List all customers in descending order of customer rating.
 select * from customers order by RATING desc;
 --92) On which days has Hoffman placed orders?
@@ -169,7 +183,9 @@ SELECT * FROM roneet.orders where AMT > (select AVG(AMT) from orders where ODATE
 select * from customers where rating >= (select rating from customers where cname = 'Grass');
 --101) Which customers have above average orders?
 select * from customers natural join orders where AMT > (select AVG(AMT) from orders);
---102) Select the total amount in orders for each salesperson for which the total is greater than the amount of the largest order in the table. 103) Give names and numbers of all salespersons that have more than one customer?
+--102) Select the total amount in orders for each salesperson for which the total is greater than the amount of the largest order in the table.
+--103) Give names and numbers of all salespersons that have more than one customer?
+select sname, snum from salespeople where snum in ( select snum from customers group by snum having count(snum)>1);
 --104) Select all salespeople by name and number who have customers in their city whom they don’t service.
 select sname, salespeople.city, cname, customers.city from salespeople, customers where customers.snum=salespeople.snum and customers.city!=salespeople.city;
 --105) Does the total amount in orders by customer in Rome and London, exceed the commission paid to salesperson in London, and New York by more than 5 times?
